7d0fd0c0dcff99a9314ce107144f6d7946527bc8
Joseph Harrison <joseph.harrison@windstream.com>
2024-11-22 19:26:02 -0500

    cleanup

--- CMakeFiles/redisearch.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "redisearch.pdb"
-  "redisearch.so"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/redisearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
